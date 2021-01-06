7d48f6c2c69801743b25e45fd13de30f801cef65
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 17:13:23 -0200

    Criado rota de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import CreateUser from './app/services/CreateUser';
+import Auth from './app/services/Auth';
 
 const routes = Router();
 
 routes.post('/users', CreateUser.store);
+routes.post('auth', Auth.store);
 
 export default routes;
