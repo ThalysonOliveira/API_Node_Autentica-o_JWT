5187a334127f49c69d90c63873e9fdcb70bc69cd
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 17:41:36 -0200

    Criado rota de listagem de usuarios

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import authConfig from './app/middlewares/authConfig';
 
 import CreateUser from './app/services/CreateUser';
+import ListUser from './app/services/ListUser';
 import Auth from './app/services/Auth';
 
 const routes = Router();
@@ -11,5 +12,6 @@ routes.post('/users', CreateUser.store);
 routes.post('/auth', Auth.authenticate);
 
 routes.use(authConfig);
+routes.get('users', ListUser.index);
 
 export default routes;
