4e9af096420385b6a1cd5bdcec5a524afe81a588
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 16:40:55 -0200

    Criado metodo de criar usuário

--- src/app/services/CreateUser.ts
+++ src/app/services/CreateUser.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import User from '../entities/User';
 
 class CreateUser {
   async store(req: Request, res: Response) {
-    return res.json({ ok: true });
+    const repository = await getRepository(User);
+    const { email, password } = req.body;
+
+    const userExist = await repository.findOne({ where: { email } });
+    if (userExist) {
+      return res.sendStatus(409);
+    }
+
+    const user = await repository.create({ email, password });
+    await repository.save(user);
+
+    return res.json(user);
   }
 }
 
