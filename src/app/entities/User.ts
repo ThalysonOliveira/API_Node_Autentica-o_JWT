01760fdb8f79a692da563044f46bead674c2d98f
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 16:55:42 -0200

    Criado função de criptografia da senha

--- src/app/entities/User.ts
+++ src/app/entities/User.ts
@@ -1,4 +1,7 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate,
+} from 'typeorm';
+import bcrypt from 'bcryptjs';
 
 @Entity('users')
 class User {
@@ -10,6 +13,12 @@ class User {
 
   @Column()
   password: string
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
 
 export default User;
